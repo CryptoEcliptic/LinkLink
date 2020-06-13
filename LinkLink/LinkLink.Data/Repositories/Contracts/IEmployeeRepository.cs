376281592b311b43e6ca2a5524e41de0845ae1c7
CryptoEcliptic <cryptoecliptic@gmail.com>
2020-06-13 17:31:17 +0300

    Created Employee repository for CRUD employees

---
+++ LinkLink/LinkLink.Data/Repositories/Contracts/IEmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using LinkLink.Data.EntityModels;
+using System.Collections.Generic;
+
+namespace LinkLink.Data.Repositories.Contracts
+{
+    public interface IEmployeeRepository
+    {
+        Employee GetById(int Id);
+
+        IEnumerable<Employee> GetEmployees();
+
+        Employee Add(Employee employee);
+
+        Employee Update(Employee employee);
+
+        Employee Delete(int id);
+    }
+}
---
+++ LinkLink/LinkLink.Data/Repositories/EmployeeRepository.cs
@@ -0,0 +1,53 @@
+﻿using LinkLink.Data.EntityModels;
+using LinkLink.Data.Repositories.Contracts;
+using System.Collections.Generic;
+
+namespace LinkLink.Data.Repositories
+{
+    class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public EmployeeRepository(ApplicationDbContext _context)
+        {
+            this._context = _context;
+        }
+
+        public Employee Add(Employee employee)
+        {
+            _context.Employees.Add(employee);
+            _context.SaveChanges();
+            return employee;
+        }
+
+        public Employee Delete(int id)
+        {
+            Employee employee = _context.Employees.Find(id);
+
+            if (employee != null)
+            {
+                _context.Employees.Remove(employee);
+                _context.SaveChanges();
+            }
+            return employee;
+        }
+
+        public Employee GetById(int id)
+        {
+            Employee employee = _context.Employees.Find(id);
+            return employee;
+        }
+
+        public IEnumerable<Employee> GetEmployees()
+        {
+            return _context.Employees;
+        }
+
+        public Employee Update(Employee employee)
+        {
+            var employeeToUpdate = _context.Employees.Attach(employee);
+            employeeToUpdate.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.SaveChanges();
+            return employee;
+        }
+    }
+}
