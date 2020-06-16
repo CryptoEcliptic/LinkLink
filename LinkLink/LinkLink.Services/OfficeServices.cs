973221d525dc4e81a16cb31af435e56b72185a64
Radoslav Vassilev <rvassilev@cleancodefactory.de>
2020-06-16 14:52:37 +0300

    Indroduced Action and service methods for Adding employee to an office

--- LinkLink/LinkLink.Services/OfficeServices.cs
+++ LinkLink/LinkLink.Services/OfficeServices.cs
@@ -2,6 +2,7 @@
 using LinkLink.Data.EntityModels;
 using LinkLink.Services.Contracts;
 using LinkLink.Services.ServiceModels;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -54,6 +55,31 @@ namespace LinkLink.Services
                  Street = o.Street
             })
             .ToList();
+
+            return serviceModel;
+        }
+
+        public async Task<ICollection<OfficeEmployeeServiceModel>> ManageEmployeeOffices(string employeeId)
+        {
+            List<Office> offices = await Task.Run(() => this._context.Offices.ToList());
+            Employee employee = await Task.Run(() => this._context.Employees
+                                                    .Include(o => o.EmployeesOffices)
+                                                    .FirstOrDefault(e => e.EmployeeId == employeeId));
+            ICollection<OfficeEmployeeServiceModel> serviceModel = new List<OfficeEmployeeServiceModel>();
+
+            foreach (var office in offices)
+            {
+                OfficeEmployeeServiceModel model = new OfficeEmployeeServiceModel()
+                {
+                     Id  = office.OfficeId,
+                     Country = office.Country,
+                     City = office.City,
+                     Street = office.Street,
+                };
+
+                //TODO Mark IsSelected only assigned officess to a user
+
+            }
 
             return serviceModel;
         }
---
+++ LinkLink/LinkLink.Services/ServiceModels/OfficeEmployeeServiceModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkLink.Services.ServiceModels
+{
+    public class OfficeEmployeeServiceModel
+    {
+        public string Id { get; set; }
+
+        public string Country { get; set; }
+
+        public string City { get; set; }
+
+        public string Street { get; set; }
+
+        public bool IsSelected { get; set; }
+    }
+}
